a5630bb0c7246fdf85c6a0e06d59108e04a09257
Mohamed Amine Amazzal <mamazzal@c2r1p3.1337.ma>
2023-05-24 21:57:02 +0100

    init siome code

--- philo.c
+++ philo.c
@@ -6,19 +6,20 @@
 /*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 14:03:22 by mamazzal          #+#    #+#             */
-/*   Updated: 2023/05/24 20:27:07 by mamazzal         ###   ########.fr       */
+/*   Updated: 2023/05/24 21:53:17 by mamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 int start_dinner(char **argv, s_philo *philo) {
+    long long time = get_current_time();
     philo->data->is_dead = 0;
     philo->data->n_philos = ft_atoi(argv[1]);
     philo->data->n_time_die = ft_atoi(argv[2]);
     philo->data->n_time_eat = ft_atoi(argv[3]);
     philo->data->n_time_sleep = ft_atoi(argv[4]);
-    philo->data->mutex = malloc(sizeof(pthread_mutex_t) * philo->data->n_philos);
+    philo->data->mutex = malloc(sizeof(pthread_mutex_t *) * philo->data->n_philos);
     pthread_mutex_init(&philo->data->print_lock, NULL);
     pthread_mutex_init(&philo->data->exit_lock, NULL);
 
@@ -37,13 +38,12 @@ int start_dinner(char **argv, s_philo *philo) {
     count = 0;
     
     while (count < philo->data->n_philos) {
-        long long time = get_current_time();
         philo[count].created_at = time;
+        philo[count].last_eat = time;
         philo[count].is_dead = 0;
         philo[count].p_id = count + 1;
         philo[count].left_mutex = count;
         philo[count].right_mutex = (count + 1) % philo->data->n_philos;
-        philo[count].last_eat = time;
         pthread_create(&philo[count].philo, NULL, philo_routine, &philo[count]);
         count++;
     }
@@ -80,7 +80,7 @@ int main(int argc, char **argv) {
 
     start_dinner(argv, philo);
     while (1) {
-        if (check_is_die(philo)) {
+        if (philo->data->is_dead) {
             return 1;
         }
     }
--- philo_routine.c
+++ philo_routine.c
@@ -6,7 +6,7 @@
 /*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 19:09:52 by mamazzal          #+#    #+#             */
-/*   Updated: 2023/05/24 20:26:51 by mamazzal         ###   ########.fr       */
+/*   Updated: 2023/05/24 21:53:41 by mamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,14 @@
 int philo_todo(s_philo *philo) {
     pthread_mutex_lock(&philo->data->mutex[philo->left_mutex]);
     print_action("has taken a fork", philo);
+    if ((get_current_time() - philo->last_eat) > philo->data->n_time_die) {
+        philo->is_dead = 1;
+        philo->data->is_dead = 1;
+        pthread_mutex_lock(&philo->data->print_lock);
+        printf("%lld %d is died\n", (get_current_time() - philo->created_at), philo->p_id);
+        pthread_mutex_unlock(&philo->data->print_lock);
+        return 1;
+    }
     pthread_mutex_lock(&philo->data->mutex[philo->right_mutex]);
     print_action("has taken a fork", philo);
     philo->last_eat = get_current_time();
@@ -33,7 +41,7 @@ void *philo_routine(void *data) {
     if (philo->p_id % 2 != 0) {
         print_action("is thinking", philo);
         sleep_time(philo->data->n_time_eat);
-    }  
+    }
     while (!philo->data->is_dead) {
         pthread_mutex_lock(&philo->data->mutex[philo->left_mutex]);
         print_action("has taken a fork", philo);
